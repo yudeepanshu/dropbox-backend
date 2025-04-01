a9cf8cf328a775d3f8085c0002a7a6f2e9b39b56
Deepanshu Waiker <waiker.deepanshu@gmail.com>
2025-04-01 23:22:33 +0530

    config update

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config(); // Load environment variables
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
